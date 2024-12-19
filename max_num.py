5a169ba4eec22c9f65b1f859550dbbdcf667a92b
Manana Ebanoidze <mananaebanoidze1998@gmail.com>
2024-12-20 00:12:50 +0400

    Add functionality to accept integer and decimal numbers from the user

--- max_num.py
+++ max_num.py
@@ -1,21 +1,22 @@
 
-numbers = []  
-
+numbers = []
 print("მითხარი რიცხვები და მათ შორის მაქსიმალურს მარტივად გაპოვნინებ.\nპროგრამიდან გასასვლელად კოდური სიტყვაა 'გასვლა'.")
 
 while True:
     num = input("\nშეიტანეთ რიცხვი: ")
-    if num =="გასვლა":
-         break
-    elif num.isdigit():
-        numbers.append(num)
-    else:
-        print("გთხოვთ შეიტანოთ რიცხვი!")
-        continue
+    if num.lower() == "გასვლა":
+        break
+    try:
+        number = float(num)
+        numbers.append(number)
+    except ValueError:
+        print("გთხოვთ შეიტანოთ  რიცხვი სწორი ფორმატით (მაგ., 3, 2.5, ან -1)!")
 
-def find_max(numbers):
-    return max(numbers)
+if numbers:    #თუ  ერთი რიცხვი მაინც შეიტანა მომხმარებელმა
+    def find_max(numbers):
+        return max(numbers)
 
-maximum = find_max(numbers)
-
-print(f"თქვენი შეტანილი რიცხვებიდან: {numbers}, მაქსიმალურია : {maximum}.")
+    maximum = find_max(numbers)
+    print(f"\nთქვენი შეტანილი რიცხვებიდან: {numbers}, მაქსიმალურია : {maximum}.")
+else:
+    print("\nრიცხვები არ არის შეტანილი.")
